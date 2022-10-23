3be8908dd5ca7d5954c03c548ce8c595efe507ad
Konstantin Atroschenko <kostay375298918971@gmail.com>
2022-10-23 23:48:17 +0300

    fix: Исправил глобальные стили для Modal

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -14,6 +14,10 @@ export function useTheme(): UseThemeResult {
     const { theme, setTheme } = useContext(ThemeContext);
 
     const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
+/* вешаем обязательно иначе будет проблемы с Portal
+ и возможно придется переопределять переменные для того что бы все работало.
+ если newTheme вешаем на самый главный корневой элемент body, то мы всего этого избежим */
+    document.body.className = newTheme;
 
     const toggleTheme = () => {
         setTheme(newTheme);
--- src/shared/config/storybook/StoreProviderDecorator/StoreProviderDecorator.tsx
+++ src/shared/config/storybook/StoreProviderDecorator/StoreProviderDecorator.tsx
@@ -3,5 +3,5 @@ import { Story } from "@storybook/react";
 import { StoreProvider } from "app/providers/StoreProvider";
 
 export const StoreProviderDecorator = (story: () => Story) => (
-    <StoreProvider>{story()}</StoreProvider>
+     <StoreProvider>{story()}</StoreProvider>
 );
