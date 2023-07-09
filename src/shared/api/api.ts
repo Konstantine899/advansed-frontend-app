c7468fc66a4f292919866ef0d271597e9cbc2846
Konstantin Atroschenko <kostay375298918971@gmail.com>
2023-07-09 14:30:19 +0300

    fix: add interceptor request adding header Authorization

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -4,7 +4,12 @@ import { USER_LOCALSTORAGE_KEY } from "shared/const/localstorage";
 
 export const $api = axios.create({
   baseURL: __API__, // указываю порт на котором находится backend
-  headers: {
-    authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) || "",
-  },
+});
+
+// добавляю interceptor который будет отрабатывать перед каждым запросом
+$api.interceptors.request.use((config) => {
+  if (config.headers) {
+    config.headers.Authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) || "";
+  }
+  return config;
 });
