f2dda4c7915a3d687dd17fbd762a6c88d25054e2
Konstantin Atroschenko <kostay375298918971@gmail.com>
2023-09-10 21:50:41 +0300

    feat: 86 StarRating. Модульный подход. Оценка статьи и профиля

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,21 +1,14 @@
 // src/pages/MainPage.tsx
-import React, { memo, useState } from "react";
+import React, { memo } from "react";
 import { useTranslation } from "react-i18next";
-import { Input } from "@/shared/ui/Input/Input";
 import { Page } from "@/widgets/Page/Page";
 
 const MainPage = memo(() => {
   const { t } = useTranslation("main");
-  const [value, setValue] = useState("");
-
-  const onChange = (currentValueInput: string) => {
-    setValue(currentValueInput);
-  };
 
   return (
     <Page>
       <div>{t("Главная страница")}</div>
-      <Input value={value} onChange={onChange} placeholder="Введите текст" />
     </Page>
   );
 });
