4be6f08bdbecd053bea9dd1f8530dccb7ea9b4c1
Konstantin Atroschenko <kostay375298918971@gmail.com>
2023-09-20 17:31:02 +0300

    feat: 104 Улучшаем сборку. Babel loader cache. Postinstall hooks

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -15,23 +15,19 @@ export function buildPlugins({
   apiUrl,
   project,
 }: BuildOptions): webpack.ProgressPlugin[] {
+  const isProd = !isDev; // если isDev false значит инвертирую в true. Тем самым подтверждаю что нахожусь в prod режиме
   const plugins = [
     new webpack.ProgressPlugin(),
     new HTMLWebpackPlugin({
       template: paths.html,
     }),
-    new MiniCssExtractPlugin({
-      filename: "css/[name].[contenthash:8].css",
-      chunkFilename: "css/[name].[contenthash:8].css",
-    }),
+
     new DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
       __API__: JSON.stringify(apiUrl),
       __PROJECT__: JSON.stringify(project),
     }),
-    new CopyPlugin({
-      patterns: [{ from: paths.locales, to: paths.buildLocales }],
-    }),
+
     new CircleDependencyPlugin({ exclude: /node_modules/, failOnError: true }),
     new ForkTsCheckerWebpackPlugin({
       typescript: {
@@ -49,6 +45,20 @@ export function buildPlugins({
     plugins.push(new ReactRefreshWebpackPlugin());
     plugins.push(new webpack.HotModuleReplacementPlugin());
     plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }));
+  }
+
+  if (isProd) {
+    plugins.push(
+      new MiniCssExtractPlugin({
+        filename: "css/[name].[contenthash:8].css",
+        chunkFilename: "css/[name].[contenthash:8].css",
+      })
+    );
+    plugins.push(
+      new CopyPlugin({
+        patterns: [{ from: paths.locales, to: paths.buildLocales }],
+      })
+    );
   }
 
   return plugins;
--- config/build/loaders/buildBabelLoader.ts
+++ config/build/loaders/buildBabelLoader.ts
@@ -8,21 +8,19 @@ interface BuildBabelLoaderProps extends BuildOptions {
 
 export function buildBabelLoader(options: BuildBabelLoaderProps) {
   const { isDev, isTsx } = options;
+  const isProd = !isDev; // если isDev false значит инвертирую в true. Тем самым подтверждаю что нахожусь в prod режиме
   return {
     test: isTsx ? /\.(jsx|tsx)$/ : /\.(js|ts)$/,
     exclude: /node_modules/,
     use: {
       loader: "babel-loader",
       options: {
+        cacheDirectory: true,
         presets: ["@babel/preset-env"],
         plugins: [
-          [
-            "i18next-extract",
-            { locales: ["ru", "en"], keyAsDefaultValue: true },
-          ],
           ["@babel/plugin-transform-typescript", { isTsx }],
           ["@babel/plugin-transform-runtime", {}],
-          isTsx && [babelRemovePropsPlugin, { props: ["data-testid"] }],
+          isTsx && isProd && [babelRemovePropsPlugin, { props: ["data-testid"] }],
           isDev && require.resolve("react-refresh/babel"),
         ].filter(Boolean),
       },
