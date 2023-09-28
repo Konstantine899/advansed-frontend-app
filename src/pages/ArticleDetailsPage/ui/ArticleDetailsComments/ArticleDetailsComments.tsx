93d1d2bcc7a108d342c3bd597b48b1ce9cad5c04
Konstantin Atroschenko <kostay375298918971@gmail.com>
2023-09-28 15:14:46 +0300

    feat: remove react-virtualized and update @reduxjs/toolkit@1.9.0 react-redux@8.0.5 --force

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsComments/ArticleDetailsComments.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsComments/ArticleDetailsComments.tsx
@@ -1,9 +1,9 @@
 // pages/ArticleDetailsPage/ui/ArticleDetailsComments/ArticleDetailsComments.tsx
 import { useTranslation } from "react-i18next";
 import { memo, Suspense, useCallback } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { Text, TextSize } from "@/shared/ui/Text";
-import { AddCommentForm, addCommentForArticle } from "@/features/addCommentForm";
+import { addCommentForArticle, AddCommentForm } from "@/features/addCommentForm";
 import { CommentList } from "@/entities/Comment";
 import { useInitialEffect } from "@/shared/lib/hooks/useInitialEffect/useInitialEffect";
 import { classNames } from "@/shared/lib/classNames/classNames";
@@ -13,44 +13,45 @@ import { fetchCommentsByArticleId } from "../../model/services/fetchCommentsByAr
 import { getArticleComments } from "../../model/slices/articleDetailsCommentsSlice";
 import { getArticleCommentsIsLoading } from "../../model/selectors/comments";
 import cls from "../ArticleDetailsPage/ArticleDetailsPage.module.scss";
+import { useAppDispatch } from "@/shared/lib/hooks/useAppDispatch/useAppDispatch";
 
 interface ArticleDetailsCommentsProps {
-  className?: string;
-  id?: string;
+    className?: string;
+    id?: string;
 }
 
 export const ArticleDetailsComments = memo(
-  (props: ArticleDetailsCommentsProps) => {
-    const { className, id } = props;
-    const { t } = useTranslation();
+    (props: ArticleDetailsCommentsProps) => {
+        const { className, id } = props;
+        const { t } = useTranslation();
 
-    const comments = useSelector(getArticleComments.selectAll);
-    const commentsIsLoading = useSelector(getArticleCommentsIsLoading);
-    const dispatch = useDispatch();
+        const comments = useSelector(getArticleComments.selectAll);
+        const commentsIsLoading = useSelector(getArticleCommentsIsLoading);
+        const dispatch = useAppDispatch();
 
-    const onSendComment = useCallback(
-      (text: string) => {
-        dispatch(addCommentForArticle(text));
-      },
-      [dispatch]
-    );
+        const onSendComment = useCallback(
+            (text: string) => {
+                dispatch(addCommentForArticle(text));
+            },
+            [dispatch]
+        );
 
-    useInitialEffect(() => {
-      dispatch(fetchCommentsByArticleId(id));
-    });
+        useInitialEffect(() => {
+            dispatch(fetchCommentsByArticleId(id));
+        });
 
-    return (
-      <VStack gap="8" max className={classNames("", {}, [className])}>
-        <Text
-          size={TextSize.L}
-          className={cls.commentTitle}
-          text={t("Комментарии")}
-        />
-        <Suspense fallback={<Loader />}>
-          <AddCommentForm onSendComment={onSendComment} />
-        </Suspense>
-        <CommentList comments={comments} isLoading={commentsIsLoading} />
-      </VStack>
-    );
-  }
+        return (
+            <VStack gap="8" max className={classNames("", {}, [className])}>
+                <Text
+                    size={TextSize.L}
+                    className={cls.commentTitle}
+                    text={t("Комментарии")}
+                />
+                <Suspense fallback={<Loader />}>
+                    <AddCommentForm onSendComment={onSendComment} />
+                </Suspense>
+                <CommentList comments={comments} isLoading={commentsIsLoading} />
+            </VStack>
+        );
+    }
 );
