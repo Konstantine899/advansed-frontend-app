ae40b3fc391614d7a413369e7004e9f42888dc55
Konstantin Atroschenko <kostay375298918971@gmail.com>
2023-09-28 13:14:20 +0300

    feat: 111 Автофикс для ESLINT правила на относительные пути

--- src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
+++ src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
@@ -4,8 +4,8 @@ import { memo, useCallback, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { classNames } from "@/shared/lib/classNames/classNames";
 import {
-  DynamicModuleLoader,
-  ReducersList,
+    DynamicModuleLoader,
+    ReducersList,
 } from "@/shared/lib/DynamicModuleLouder/DynamicModuleLoader";
 import { useAppDispatch } from "@/shared/lib/hooks/useAppDispatch/useAppDispatch";
 import { Text, TextAlign, TextSize } from "@/shared/ui/Text";
@@ -21,137 +21,137 @@ import { ArticleTextBlockComponent } from "../../ui/ArticleTextBlockComponent/Ar
 import { ArticleImageBlockComponent } from "../../ui/ArticleImageBlockComponent/ArticleImageBlockComponent";
 import { ArticleBlock } from "../../model/types/article";
 import {
-  getArticleDetailsData,
-  getArticleDetailsIsError,
-  getArticleDetailsIsLoading,
+    getArticleDetailsData,
+    getArticleDetailsIsError,
+    getArticleDetailsIsLoading,
 } from "../../model/selectors/articleDetails";
 import { fetchArticleById } from "../../model/services/fetchArticleById/fetchArticleById";
 import { articleDetailsReducer } from "../../model/slice/articleDetailsSlice";
 import cls from "./ArticleDetails.module.scss";
 
 interface ArticleDetailsProps {
-  className?: string;
-  id?: string;
+    className?: string;
+    id?: string;
 }
 
 const reducers: ReducersList = { articleDetails: articleDetailsReducer };
 
 export const ArticleDetails = memo((props: ArticleDetailsProps) => {
-  const { className, id } = props;
-  const { t } = useTranslation();
-  const dispatch = useAppDispatch();
+    const { className, id } = props;
+    const { t } = useTranslation();
+    const dispatch = useAppDispatch();
 
-  const isLoading = useSelector(getArticleDetailsIsLoading);
-  const article = useSelector(getArticleDetailsData);
-  const error = useSelector(getArticleDetailsIsError);
+    const isLoading = useSelector(getArticleDetailsIsLoading);
+    const article = useSelector(getArticleDetailsData);
+    const error = useSelector(getArticleDetailsIsError);
 
-  const renderBlock = useCallback((block: ArticleBlock) => {
-    switch (block.type) {
-      case ArticleBlockType.CODE: {
-        return (
-          <ArticleCodeBlockComponent
-            key={block.id}
-            className={cls.block}
-            block={block}
-          />
-        );
-      }
-      case ArticleBlockType.TEXT: {
-        return (
-          <ArticleTextBlockComponent
-            key={block.id}
-            className={cls.block}
-            block={block}
-          />
-        );
-      }
+    const renderBlock = useCallback((block: ArticleBlock) => {
+        switch (block.type) {
+            case ArticleBlockType.CODE: {
+                return (
+                    <ArticleCodeBlockComponent
+                        key={block.id}
+                        className={cls.block}
+                        block={block}
+                    />
+                );
+            }
+            case ArticleBlockType.TEXT: {
+                return (
+                    <ArticleTextBlockComponent
+                        key={block.id}
+                        className={cls.block}
+                        block={block}
+                    />
+                );
+            }
 
-      case ArticleBlockType.IMAGE: {
-        return (
-          <ArticleImageBlockComponent
-            key={block.id}
-            className={cls.block}
-            block={block}
-          />
+            case ArticleBlockType.IMAGE: {
+                return (
+                    <ArticleImageBlockComponent
+                        key={block.id}
+                        className={cls.block}
+                        block={block}
+                    />
+                );
+            }
+            default:
+                return null;
+        }
+    }, []);
+
+    useEffect(() => {
+        if (__PROJECT__ !== "storybook") {
+            dispatch(fetchArticleById(id));
+        }
+    }, [dispatch, id]);
+
+    /* Условие загрузки данных */
+    let content;
+    if (isLoading) {
+        // pending
+        content = (
+            <>
+                <Skeleton
+                    className={cls.avatar}
+                    width={200}
+                    height={200}
+                    border="50%"
+                />
+                <Skeleton className={cls.title} width={300} height={32} />
+                <Skeleton className={cls.skeleton} width={600} height={24} />
+                <Skeleton className={cls.skeleton} width="100%" height={200} />
+                <Skeleton className={cls.skeleton} width="100%" height={200} />
+            </>
         );
-      }
-      default:
-        return null;
+    } else if (error) {
+        // error
+        // eslint-disable-next-line i18next/no-literal-string
+        content = (
+            <Text
+                align={TextAlign.CENTER}
+                title={t(`Произошла ошибка при загрузке статьи`)}
+            />
+        );
+    } else {
+        // success
+        // eslint-disable-next-line i18next/no-literal-string
+        content = (
+            <>
+                <HStack justify="center" max className={cls.avatarWrapper}>
+                    <Avatar size={200} src={article?.img} className={cls.avatar} />
+                </HStack>
+                <VStack gap="8" max>
+                    <Text
+                        className={cls.title}
+                        title={article?.title}
+                        text={article?.subtitle}
+                        size={TextSize.L}
+                    />
+                    <HStack gap="8" className={cls.articleInfo}>
+                        <Icon className={cls.icon} Svg={EyeIcon} />
+                        <Text text={String(article?.views)} />
+                    </HStack>
+                    <HStack gap="8" className={cls.articleInfo}>
+                        <Icon className={cls.icon} Svg={CalendarIcon} />
+                        <Text text={article?.createdAt} />
+                    </HStack>
+                </VStack>
+
+                {article?.blocks.map(renderBlock)}
+            </>
+        );
     }
-  }, []);
 
-  useEffect(() => {
-    if (__PROJECT__ !== "storybook") {
-      dispatch(fetchArticleById(id));
-    }
-  }, [dispatch, id]);
-
-  /* Условие загрузки данных */
-  let content;
-  if (isLoading) {
-    // pending
-    content = (
-      <>
-        <Skeleton
-          className={cls.avatar}
-          width={200}
-          height={200}
-          border="50%"
-        />
-        <Skeleton className={cls.title} width={300} height={32} />
-        <Skeleton className={cls.skeleton} width={600} height={24} />
-        <Skeleton className={cls.skeleton} width="100%" height={200} />
-        <Skeleton className={cls.skeleton} width="100%" height={200} />
-      </>
+    return (
+        <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
+            <VStack
+                gap="16"
+                className={classNames(cls.ArticleDetails, {}, [className])}
+            >
+                {/* eslint-disable-next-line i18next/no-literal-string */}
+                {content}
+            </VStack>
+        </DynamicModuleLoader>
     );
-  } else if (error) {
-    // error
-    // eslint-disable-next-line i18next/no-literal-string
-    content = (
-      <Text
-        align={TextAlign.CENTER}
-        title={t(`Произошла ошибка при загрузке статьи`)}
-      />
-    );
-  } else {
-    // success
-    // eslint-disable-next-line i18next/no-literal-string
-    content = (
-      <>
-        <HStack justify="center" max className={cls.avatarWrapper}>
-          <Avatar size={200} src={article?.img} className={cls.avatar} />
-        </HStack>
-        <VStack gap="8" max>
-          <Text
-            className={cls.title}
-            title={article?.title}
-            text={article?.subtitle}
-            size={TextSize.L}
-          />
-          <HStack gap="8" className={cls.articleInfo}>
-            <Icon className={cls.icon} Svg={EyeIcon} />
-            <Text text={String(article?.views)} />
-          </HStack>
-          <HStack gap="8" className={cls.articleInfo}>
-            <Icon className={cls.icon} Svg={CalendarIcon} />
-            <Text text={article?.createdAt} />
-          </HStack>
-        </VStack>
-
-        {article?.blocks.map(renderBlock)}
-      </>
-    );
-  }
-
-  return (
-    <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
-      <VStack
-        gap="16"
-        className={classNames(cls.ArticleDetails, {}, [className])}
-      >
-        {/* eslint-disable-next-line i18next/no-literal-string */}
-        {content}
-      </VStack>
-    </DynamicModuleLoader>
-  );
 });
